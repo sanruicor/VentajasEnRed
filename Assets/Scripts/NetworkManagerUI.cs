ac142bf845d82215fa379de024ee353c5e4934be
AdelaSantallaRuizDeCortazar <sanruiade@gmail.com>
2026-05-20 11:06:21 +0200

    Players con movimiento y salto básico

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,16 +1,22 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private NetworkManager m_Net;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake() => m_Net = GetComponent<NetworkManager>();
+
+    private void OnGUI()
     {
-        
+        if (!m_Net.IsClient && !m_Net.IsServer)
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 200, 80));
+            if (GUILayout.Button("Host"))   m_Net.StartHost();
+            if (GUILayout.Button("Client")) m_Net.StartClient();
+            if (GUILayout.Button("Server")) m_Net.StartServer();
+            GUILayout.EndArea();
+            return;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,79 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float moveSpeed = 5f;
+    private float advantageSpeed = 10f;
+    private float disavantageSpeed = 2f;
+    private float jumpForce = 6f;
+    private float timeToAdvantageOrDisavantage = 20f;
+    private float effectDuration = 10f;
+    private float panelLimit = 4.5f;
+
+    private PlayerInputActions m_Input;
+    private Rigidbody m_Rb;
+
+
+
+    private bool IsGrounded() =>
+        Physics.Raycast(transform.position, Vector3.down, 1.1f);
+
+    public override void OnNetworkSpawn()
     {
-        
+        m_Rb = GetComponent<Rigidbody>();
+
+        if (!IsOwner)
+        {
+            enabled = false;
+            return;
+        }
+
+        if (IsServer)
+        {
+            transform.position = GetRandomPosition();
+        }
+
+        m_Input = new PlayerInputActions();
+        m_Input.Player.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = m_Input.Player.Move.ReadValue<Vector2>();
+        bool jump = m_Input.Player.Jump.WasPressedThisFrame();
+
+        SendInputServerRpc(moveInput, jump);
+    }
+
+    private static Vector3 GetRandomPosition()
+    {
+        return new Vector3(Random.Range(-4f, 4f), 1f, Random.Range(-4f, 4f));
+    }
+
+    private void ApplyMovement(Vector2 moveInput, bool jump)
+    {
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed;
+        m_Rb.linearVelocity = new Vector3(move.x, m_Rb.linearVelocity.y, move.z);
+
+        if (jump && IsGrounded())
+        {
+            m_Rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void ClampPosition()
+    {
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, -panelLimit, panelLimit);
+        position.z = Mathf.Clamp(position.z, -panelLimit, panelLimit);
+        transform.position = position;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SendInputServerRpc(Vector2 moveInput, bool jump)
+    {
+        ApplyMovement(moveInput, jump);
+        ClampPosition();
     }
 }
